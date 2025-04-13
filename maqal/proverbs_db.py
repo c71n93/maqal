20f410fa65b7bab72019c17f4bfe368dd7615d4e
Roman Korostinskiy <70313618+c71n93@users.noreply.github.com>
2025-04-14 00:34:53 +0300

    Remove modified time

--- maqal/proverbs_db.py
+++ maqal/proverbs_db.py
@@ -9,6 +9,6 @@ def all_proverbs():
         kazakh_text = proverb.kazakh_text
         russian_text = proverb.russian_text
         author = proverb.author if proverb.author is not None else "admin"
-        modified_at = proverb.modified_at.strftime("%d.%m.%Y %H:%m")
+        modified_at = proverb.modified_at.strftime("%d.%m.%Y")
         proverbs.append([cnt, kazakh_text, russian_text, author, modified_at])
     return proverbs
